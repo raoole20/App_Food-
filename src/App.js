8fbe4ad77ce2557a4a73c61c5ce9794ec9df9407
raoole <espinaraul@gmail.com>
2022-07-11 11:23:52 -0400

    Staring this proyect app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/layout/Header"
+import Meals from "./components/meals/Meals";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Meals />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/meals/Meals.js
@@ -0,0 +1,14 @@
+import AvilableMeals from "./AvilableMeals"
+import MealsSummary from "./MealsSummary"
+
+const Meals = () => {
+
+    return (    
+        <>
+            <MealsSummary />
+            <AvilableMeals />
+        </>
+    )
+}
+
+export default Meals
